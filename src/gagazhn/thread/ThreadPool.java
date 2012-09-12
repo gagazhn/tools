a55c18227015adb79ba80f987bfd55a386ecf4d2
gagazhn <gaga.zhn@gmail.com>
2012-09-12 17:20:03 +0800

    add thread

---
+++ src/gagazhn/thread/ThreadPool.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package gagazhn.thread;
+
+/**
+ * @author gagazhn
+ *
+ */
+public class ThreadPool {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
